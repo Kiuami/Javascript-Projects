4c8e73851f6c2e7e6e1d1e3e27b4bd84230bbef8
Kiuami <z1JgUB6Z9oFm06BA7125Z@hotmail.com>
2023-01-19 22:25:42 -0800

    Got basic Javascript set up

---
+++ Javascript-Projects/Project1_expressions_alert/main.js
@@ -0,0 +1,9 @@
+alert("It's Working!");
+var A = "This is a string printed using Document Write!";
+var B = "This is a string printed using Window.Alert()!";
+var C = "This is a string printed with an apostrophe. It\'s a escape character! (The apostrophe is \')";
+document.write("This is a Document Write!");
+document.write(A);
+document.write(C);
+window.alert(B);
+
