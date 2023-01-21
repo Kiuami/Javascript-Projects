e101c1ef247e686a5261784e14716362a7c861c7
Kiuami <z1JgUB6Z9oFm06BA7125Z@hotmail.com>
2023-01-20 20:58:26 -0800

    Added comments and expression in javascript

--- Javascript-Projects/Project1_expressions_alert/main.js
+++ Javascript-Projects/Project1_expressions_alert/main.js
@@ -1,9 +1,13 @@
-alert("It's Working!");
-var A = "This is a string printed using Document Write!";
-var B = "This is a string printed using Window.Alert()!";
-var C = "This is a string printed with an apostrophe. It\'s a escape character! (The apostrophe is \')";
-document.write("This is a Document Write!");
-document.write(A);
-document.write(C);
-window.alert(B);
+alert("It's Working!"); // Creates an alert pop-up with text
+var A = "This is a string printed using Document Write!"; // Variable assigned to a string
+var B = "This is a string printed using Window.Alert()!"; // Variable assigned to a string
+var C = "This is a string printed with an apostrophe. It\'s a escape character! (The apostrophe is \')"; // Variable assigned to a string
+var D = 4 // Variable assigned to a number
+var E = 9 // Variable assigned to a number
+document.write("This is a Document Write!"); // Write a string into the current HTML document
+document.write(A); // Write a variable value into the current HTML document
+document.write(C); // Write a variable value into the current HTML document
+document.write(D + E); // Write a variable value into the current HTML document
+document.write(3 + 3); // Write an expression result into the current HTML document
+window.alert(B); // Write a variable value into the current HTML document
 
