0f0d178268c201347f3be576a4a62ae2b501595d
Kiuami <z1JgUB6Z9oFm06BA7125Z@hotmail.com>
2023-01-26 19:14:57 -0800

    added dictionary function

--- Javascript-Projects/Project4_dictionaries/main.js
+++ Javascript-Projects/Project4_dictionaries/main.js
@@ -1,31 +1,24 @@
+var nann = 0/0;
 
+document.write(nann);
 
-var F = 5; // Variable assigned to a number
-F++; // Variable with an increment operator
-document.write(F); // Show the results of the increment
+document.write(isNaN(0/0));
 
-var G = 5; // Variable assigned to a number
-G--; // Variable with a decrement operator
-document.write(G); // Show the results of the decrement
+document.write(isNaN(1/1));
 
+document.write(typeof "Word");
 
-document.write(Math.random()); // Print a random number
+document.write(typeof 3);
 
-document.write(math.random() * 100); // Print a random number mutliplied by 100
+document.write("10" + 5);
 
-document.write(Math.SQRT8); // Find the square root of 8.
-
-document.write(Math.ceil(8.7)); // Round 8.7 UP to it's nearest integer.
-
-function ButtonClick() { // Function prototyping, defining a Function
-    var ParagraphString = "I changed the text!"; // Variable assigned to a string
-    document.getElementById("ParagraphText").innerHTML = ParagraphString; // Calling a Method to change the text of an Element
+function BaseInfinity() { // Function prototyping, defining a Function
+    document.getElementById("InfinityParagraph").innerHTML = 9E654; // Calling a Method to change the text of an Element
 }
 
-function ButtonConcatenate() { // Function prototyping, defining a Function
-    var ParagraphString = "I changed the text"; // Variable assigned to a string
-    ParagraphString += " by using a function!"; // Concatenating a string
-    document.getElementById("Button_Concatenate").innerHTML = ParagraphString; // Calling a Method to change the text of an Element
+function NegativeInfinity() { // Function prototyping, defining a Function
+
+    document.getElementById("NegativeInfinityParagraph").innerHTML = -9E654; // Calling a Method to change the text of an Element
 }
 
 function Dictionary() { // Function prototyping, defining a Function
