08c66115a117158a07273fe37d1a938a9718265e
Kiuami <z1JgUB6Z9oFm06BA7125Z@hotmail.com>
2023-01-26 21:31:54 -0800

    Added lots of operator testing

--- Javascript-Projects/Project4_dictionaries/main.js
+++ Javascript-Projects/Project4_dictionaries/main.js
@@ -12,6 +12,40 @@ document.write(typeof 3);
 
 document.write("10" + 5);
 
+document.write(10 > 5);
+
+document.write(10 < 2);
+
+console.log(2 + 2); // Press F12 to open the console and see this
+
+console.log(12 < (2 + 2)); // Press F12 to open the console and see this
+
+document.write(10 == 1);
+
+document.write(3 == 3);
+
+document.write(3 === 3);
+
+document.write(3 === "String");
+
+document.write(3 === "3");
+
+document.write(3 === 2);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
+
+document.write(!(5 > 10));
+
+document.write(!(20 > 10));
+
 function BaseInfinity() { // Function prototyping, defining a Function
     document.getElementById("InfinityParagraph").innerHTML = 9E654; // Calling a Method to change the text of an Element
 }
