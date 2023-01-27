0f0604203941f2eacab24985b98a746b4b10b113
Kiuami <z1JgUB6Z9oFm06BA7125Z@hotmail.com>
2023-01-26 21:42:45 -0800

    comments

--- Javascript-Projects/Project4_dictionaries/main.js
+++ Javascript-Projects/Project4_dictionaries/main.js
@@ -1,51 +1,49 @@
-var nann = 0/0;
+var nann = 0/0; // Declare NaN as a value in a variable
 
-document.write(nann);
+document.write(nann); // Print the value of NaN
 
-document.write(isNaN(0/0));
+document.write(isNaN(0/0)); // Check if it is NaN or not
 
 document.write(isNaN(1/1));
 
-document.write(typeof "Word");
+document.write(typeof "Word"); // Print the data type of the argument
 
-document.write(typeof 3);
+document.write(typeof 3); // Print the data type of the argument
 
-document.write("10" + 5);
+document.write("10" + 5); // Print true or false based on a comparison
 
-document.write(10 > 5);
+document.write(10 > 5); // Print true or false based on a comparison
 
-document.write(10 < 2);
+document.write(10 < 2); // Print true or false based on a comparison
 
 console.log(2 + 2); // Press F12 to open the console and see this
 
 console.log(12 < (2 + 2)); // Press F12 to open the console and see this
 
-document.write(10 == 1);
+document.write(10 == 1); // Print true or false based on a comparison
 
-document.write(3 == 3);
+document.write(3 == 3); // Print true or false based on a comparison
 
-document.write(3 === 3);
+document.write(3 === 3); // Print true or false based on a triple comparison
 
-document.write(3 === "String");
+document.write(3 === "String"); // Print true or false based on a triple comparison
 
-document.write(3 === "3");
+document.write(3 === "3"); // Print true or false based on a triple comparison
 
-document.write(3 === 2);
+document.write(3 === 2); // Print true or false based on a triple comparison
 
-document.write(5 > 2 && 10 > 4);
+document.write(5 > 2 && 10 > 4); // Print true or false if both comparisons are correct
 
-document.write(5 > 10 && 10 > 4);
+document.write(5 > 10 && 10 > 4); // Print true or false if both comparisons are correct
 
-document.write(5 > 10 && 10 > 4);
+document.write(5 > 10 || 10 > 4); // Print true or false if at least one comparisons is correct
 
-document.write(5 > 10 || 10 > 4);
+document.write(5 > 10 || 10 > 20); // Print true or false if at least one comparisons is correct
 
-document.write(5 > 10 || 10 > 20);
+document.write(!(5 > 10)); // Print true or false if the inverse is true
 
-document.write(!(5 > 10));
-
-document.write(!(20 > 10));
-
+document.write(!(20 > 10)); // Print true or false if the inverse is true
+ 
 function BaseInfinity() { // Function prototyping, defining a Function
     document.getElementById("InfinityParagraph").innerHTML = 9E654; // Calling a Method to change the text of an Element
 }
