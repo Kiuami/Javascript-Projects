e3bce7eb0c79b6642d61bf552b88afeea33caf1c
Kiuami <z1JgUB6Z9oFm06BA7125Z@hotmail.com>
2023-01-31 21:14:17 -0800

    fixed

--- Javascript-Projects/project7_/main.js
+++ Javascript-Projects/project7_/main.js
@@ -1,53 +1,3 @@
-var VarA = 42;
-
-function Ride_Function() { // Function prototyping
-    var Height, Can_ride; // Variable definition
-    Height = document.getElementById("Height").value; // Finding the element to edit
-    Can_ride = (Height < 52) ? "You are too short":"You are tall enough"; // Value comparison with a conditional operator
-    document.getElementById("Ride").innerHTML = Can_ride + " to ride."; // Change the text to the proper value
-}
-
-function Vehicle(Make, Model, Year, Color) { // Create a class
-    this.Vehicle_Make = Make; // Create class properties
-    this.Vehicle_Model = Model; // Create class properties
-    this.Vehicle_Year = Year; // Create class properties
-    this.Vehicle_Color = Color; // Create class properties
-} 
-var Jack = new Vehicle("Dodge", "Viper", 2020, "Red") // Create class constructor
-var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black")
-var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard")
-
-function myFunction() { // Function prototyping
-    document.getElementById("New_and_This").innerHTML = // Set the text to class instance properties
-    "Erik drives a" + Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +
-    " manufactured in " + Erik.Vehicle_Year;
-}
-
-function get_Date() { // Function prototyping
-    if (new Date().getHours() < 18 ) { // Get the current time in hours
-    document.getElementById("Greeting").innerHTML = "How are you today?";
-    }
-}
-
-function if_statement() { // Function prototyping
-    var VarB = 2;
-    if (1 < VarB) { // Conditional statement comparison
-        document.write("2 is greater than 1")
-    }
-
-}
-
-function Age_Function() { // Function prototyping
-    Age = document.getElementById("Age").value;
-    if (Age >= 18) { // Conditional statement comparison
-        Vote = "You are old enough to vote!";
-    }
-    else {
-        Vote = "You are not old enough to vote!";
-    }
-    document.getElementById("How_old_are_you?").innerHTML = Vote;
-}
-
 function Time_function() { // Function prototyping
     var Time = new Date().getHours; // Get the current time in hours
     var Reply;
@@ -57,15 +7,60 @@ function Time_function() { // Function prototyping
     else {
         Reply = "It is evening time.";
     }
-    document.getElementById("Time_of_day").innerHTML = Reply;
+    document.getElementById("Time_of_day").innerHTML = Reply; // Set the text of the element to the variable
 }
 
-function ErrorFunction() { // Function prototyping
-    var VarB = 2;
-    if (1 < VarB) { // Conditional statement comparison
-        document.write("2 is greater than 1")
-    }
 
+function ConcatenateFunc() { // Function prototyping
+    var part1 = "This is "; // Variable assignment
+    var part2 = "now a ";
+    var part3 = "complete sentence. ";
+    var sentence = part1.concat(part2, part3); // concentate the text together
+    document.getElementById("Concatenate").innerHTML = sentence; // Set the text of the element to the variable
 }
 
-console.log(ErrorFunction); //Console Log command
+
+function SliceFunc() { // Function prototyping
+    var Sentence = "Hello, this is a long sentence." // Variable assignment
+    var Section = Sentence.slice(12,20); // slice a part of the sentence off
+    document.getElementById("Slicing").innerHTML = Section; // Set the text of the element to the variable
+}
+
+
+function toUpperCaseFunc() { // Function prototyping
+    var Example = "This is an UpperCase SentencE!"; // Variable assignment
+    var Example2 = Example.toUpperCase(); // convert the sentence to uppercase entirely
+    document.getElementById("Uppercasing").innerHTML = Example2; // Set the text of the element to the variable
+}
+
+
+function SearchFunc() { // Function prototyping
+    var Example = "This is a sentence for searching."; // Variable assignment
+    var Example2 = Example.search("searching."); // list the character position of the word
+    document.getElementById("Searching").innerHTML = Example2; // Set the text of the element to the variable
+}
+
+
+function StringFunc() { // Function prototyping
+    var Number = 0983712; // Variable assignment
+    document.getElementById("NumberString").innerHTML = Number.toString(); // Convert the integer to a string
+}
+
+
+function PrecisionString() { // Function prototyping
+    var Number = 0983712.16439721; // Variable assignment
+    document.getElementById("Precision").innerHTML = Number.toPrecision(10); // print the number but only to the 10th place
+}
+
+
+function ToFixedFunc() { // Function prototyping
+    var Number = 0983712.16439721; // Variable assignment
+    var Number2 = Number.toFixed(8); // print the number but only by the 8th place
+    document.getElementById("ToFixedFunc").innerHTML = Number2; // Set the text of the element to the variable
+}
+
+function ValueOfFunc() { // Function prototyping
+    var String = "0983712.16439721"; // Variable assignment
+    var String2 = String.valueOf(); // print the raw value of the string
+    document.getElementById("ValueOfFunc").innerHTML = String2; // Set the text of the element to the variable
+}
