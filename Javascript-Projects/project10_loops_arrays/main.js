d5286c3c5f4d45bf6cd58069d51a0b81b6da9d42
Kiuami <z1JgUB6Z9oFm06BA7125Z@hotmail.com>
2023-02-01 21:31:00 -0800

    added commenting

--- Javascript-Projects/project10_loops_arrays/main.js
+++ Javascript-Projects/project10_loops_arrays/main.js
@@ -1,35 +1,35 @@
-function WhileLoop() {
+function WhileLoop() { // function prototyping
 var String = "";
 var X = 1;
-while (X < 11) {
-    String += "<br>" + X;
-    X++;
+while (X < 11) {  // If X is not greater then 11 then execute the operations below
+    String += "<br>" + X; // Concatenate string by printing the value of the index that X is equal to and add a line break.
+    X++; // Increment X by 1
 }
 
 document.getElementById("WhileLoopA").innerHTML = String;
 
 }
 
-function ForLoop() {
-    var Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function ForLoop() { // function prototyping
+    var Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Array
     var String = "";
     var X
-    for (X = 0; X < Numbers.length; X++) {
-    String += Numbers[X] + "<br>";
+    for (X = 0; X < Numbers.length; X++) { // for loop, it increase by 1 every time X is smaller then the length of the Array
+    String += Numbers[X] + "<br>"; // Concatenate string by printing the value of the index that X is equal to and add a line break.
     }
     
 document.getElementById("ForLoopA").innerHTML = String;
     
     }
 
-function ArrayObject() {
-   var ArrayA = [];
-   ArrayA[0] = "Walking";
+function ArrayObject() { // function prototyping
+   var ArrayA = []; // Array creation
+   ArrayA[0] = "Walking"; // Array object creation
    ArrayA[1] = "Jogging";
    ArrayA[2] = "Running";
    ArrayA[3] = "Sprinting";
 
-   let State = ArrayA[3];
+   let State = ArrayA[3]; // Make state temporarily equal to the value of index 3.
 
         
 document.getElementById("ArrayObjectA").innerHTML = State;
